617fedf1ecca9fb983592c8b48b34652dc8114ee
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2026-02-02 22:23:00 +0100

    Lecture 13: - Created AuraGameMode class and its blueprint - implemented SpringArm and Camera component in AuraCharacter class

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,40 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+/* CONSTRUCTOR
+ * Step 1 - Setup SpringArm component
+ * Step 2 - Setup Camera component
+ * Step 3 - Setup CharacterMovement component (inherited from Character class)
+ * Step 4 - Deactivate Controller Rotation on the class
+ */
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArmComponent->SetRelativeRotation(FRotator(-45.f, 0.f, 0.0f));
+	SpringArmComponent->TargetArmLength = 750.f;
+	SpringArmComponent->bUsePawnControlRotation = false;
+	SpringArmComponent->bEnableCameraLag = true;
+	SpringArmComponent->bInheritPitch = false;
+	SpringArmComponent->bInheritRoll = false;
+	SpringArmComponent->bInheritYaw = false;
+	SpringArmComponent->SetupAttachment(RootComponent);
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->bUsePawnControlRotation = false;
+	Camera->SetupAttachment(SpringArmComponent);
+	
+	
+}
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -6,6 +6,8 @@
 #include "Character/AuraCharacterBase.h"
 #include "AuraCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
 /**
  * 
  */
@@ -13,5 +15,17 @@ UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
+	
+public:
+	AAuraCharacter();
+	
+private:
+	UPROPERTY(EditAnywhere, Category="Camera")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	
+	UPROPERTY(EditAnywhere, Category="Camera")
+	TObjectPtr<UCameraComponent> Camera;
+	
+	
 	
 };
