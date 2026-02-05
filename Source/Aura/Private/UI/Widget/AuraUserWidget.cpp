f27991e34fdf7c22262b6639b4f3015246879ee5
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2026-02-05 23:06:40 +0100

    Lecture 29: - created WidgetController class (UObject) to manage all the data - created AuraUserWidget class with reference to WidgetController used as base class for widget to show data

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+/* SET WIDGET CONTROLLER()
+ * Step.1 - declare this function
+ * Step.2 - set the WidgetController member with argument passed calling function
+ * Step.3 - call function WidgetControllerSet() ;
+ */
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet(); // the moment we call this function we know the WidgetController exist
+}
